0b1ed8ab7c44d43bcb91fd4e362958c00b0910c1
Sydney Devaney <Sydney@Sydneys-MacBook-Air.local>
2021-01-21 12:20:39 -0500

    Lab 1

---
+++ lab1/Sydney.js
@@ -0,0 +1,11 @@
+import Person from './Person.js.js';
+
+
+class Sydney extends Person {
+  constructor(){
+
+    super("Sydney", "8442f5");
+  }
+}
+
+export default Sydney;
---
+++ lab1/index.js
@@ -0,0 +1,5 @@
+
+import Sydney from './Sydney.js.js';
+
+const p = new Sydney("Sydney", "8442f5");
+p.speak();
