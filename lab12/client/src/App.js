9ce7cf33160daaeef73d20cdac0088c2febb13ee
SydneyDevaney1 <srdevaney@knights.ucf.edu>
2021-04-24 12:48:33 -0400

    lab 12 almost done

---
+++ lab12/client/src/App.js
@@ -0,0 +1,17 @@
+
+import CreatePart from './components/CreatePart';
+import DeleteBook from './DeleteBook';
+
+function App() {
+  return (
+    <div className="App">
+      <CreatePart />
+      <br/>
+      <br/>
+      <br />
+      <DeleteBook />
+    </div>
+  );
+}
+
+export default App;
